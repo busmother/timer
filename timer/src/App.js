c323ed1a36a7bf32b04781fbfe4996d83243ae1a
Ian Marshall <imarshall14@gmail.com>
2021-08-15 20:03:32 -0400

    build out functions for App component

--- timer/src/App.js
+++ timer/src/App.js
@@ -6,11 +6,40 @@ import Control from './control.js'
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { timer: 0, paused: true}
+  };
+
+  tick = () => {
+    this.setState({timer : this.state.timer +1})
+  }
+
+  startTimer = () => {
+    this.interval = setInterval(this.tick,1000);
+    this.setState({paused : false})
+  }
+
+  pauseTimer = () => {
+    clearInterval( this.interval );
+    this.setState({paused : true})
+  }
+
+  reset = () => {
+    this.setState({timer : 0, paused : true });
+    clearInterval( this.interval )
+  }
+
   render() {
       return (
       <div className="App">
-        <Timer/>
-        <Reset/>
+        <Timer time={this.state.timer}/>
+        <Control 
+          paused={this.state.paused}
+          start={this.startTimer}
+          pause={this.pauseTimer}
+          />
+        <Reset onClickReset={this.reset}/>
       </div>
     );
   }
